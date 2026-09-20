ca577c1c34ff9b5dd09822fafd6af7fd0a43ffc4
nihardas <cipherxo1@protonmail.com>
2026-09-20 18:56:24 +0530

    [python-lab](feat): Added logic for practical 3

---
+++ python-lab/practical-3.py
@@ -0,0 +1,10 @@
+a = int(input("Enter a:"))
+b = int(input("Enter b:"))
+c = int(input("Enter c:"))
+
+if a > b and a > c:
+    print("a is greatest")
+elif b > a and b > c:
+    print("b is greatest")
+else:
+    print("c is greatest")
